807d42a4a01cb709d887f109dae0ddffdc1035da
kakcura <korhan.akcura@codefor.ca>
2021-03-22 05:38:43 -0400

    Sprint 2 - Add the sprint wireframes into the prototype

--- src/views/MembersPortal.vue
+++ src/views/MembersPortal.vue
@@ -1,30 +1,28 @@
 <template>
   <div>
-    <img id="screen1" src="https://code-for-canada.github.io/transition-centre/sprints/sprint1/screens/446151591.png"
+    <img id="screen1" src="https://code-for-canada.github.io/transition-centre/sprints/sprint2/img/members-portal/447783646.png"
       alt="Screen" usemap="#screenmap1" width="1441px" v-show="showScreen1">
 
     <map name="screenmap1">
-      <area shape="rect" coords="150,705,899,747" alt="Screen2" onfocus="focus();" @click="firstExpandClicked">
-      <area shape="rect" coords="150,856,899,899" alt="Screen3" @click="secondExpandClicked">
       <area shape="rect" coords="1165,115,1292,165" alt="Main" href="../../">
+      <router-link to="/plan">
+        <area shape="rect" coords="148,631,318,653" alt="ChecklistPlan">
+      </router-link>
+      <area shape="rect" coords="150,705,899,747" alt="Screen2" @click="expand">
     </map>
 
-    <img id="screen2" src="https://code-for-canada.github.io/transition-centre/sprints/sprint1/screens/446151592.png"
+    <img id="screen2" src="https://code-for-canada.github.io/transition-centre/sprints/sprint2/img/members-portal/447783647.png"
       alt="Screen" usemap="#screenmap2" width="1441px" v-show="showScreen2">
 
     <map name="screenmap2">
-      <area shape="rect" coords="150,705,899,747" alt="Screen2" @click="firstExpandClicked">
-      <area shape="rect" coords="150,1031,899,1074" alt="Screen3" @click="secondExpandClicked">
       <area shape="rect" coords="1165,115,1292,165" alt="Main" href="../../">
-    </map>
-
-    <img id="screen3" src="https://code-for-canada.github.io/transition-centre/sprints/sprint1/screens/446151593.png"
-      alt="Screen" usemap="#screenmap3" width="1441px" v-show="showScreen3">
-
-    <map name="screenmap3">
-      <area shape="rect" coords="150,705,899,747" alt="Screen2" @click="firstExpandClicked">
-      <area shape="rect" coords="150,856,899,899" alt="Screen3" @click="secondExpandClicked">
-      <area shape="rect" coords="1165,115,1292,165" alt="Main" href="../../">
+      <router-link to="/plan">
+        <area shape="rect" coords="148,631,318,653" alt="ChecklistPlan">
+      </router-link>
+      <area shape="rect" coords="150,705,899,747" alt="Screen1" @click="expand">
+      <router-link to="/plan">
+        <area shape="rect" coords="174,879,578,900" alt="ChecklistPlan">
+      </router-link>
     </map>
   </div>
 </template>
@@ -36,29 +34,17 @@
        return {
           showScreen1: true,
           showScreen2: false,
-          showScreen3: false
         }
     },
     methods: {
-      hideAll() {
-        this.showScreen1 = false;
-        this.showScreen2 = false;
-        this.showScreen3 = false;
-      },
-      expand(is_screen_element_visible) {
-        const is_visible = this[is_screen_element_visible];
-        this.hideAll();
-        if (is_visible) {
-          this.showScreen1 = true;
+      expand() {
+        if (this.showScreen1) {
+          this.showScreen1 = false;
+          this.showScreen2 = true;
         } else {
-          this[is_screen_element_visible] = true;
+          this.showScreen2 = false;
+          this.showScreen1 = true;
         }
-      },
-      firstExpandClicked() {
-        this.expand('showScreen2');
-      },
-      secondExpandClicked() {
-        this.expand('showScreen3');
       }
     }
   }
---
+++ src/views/SmartGoals.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <img id="screen1" src="https://code-for-canada.github.io/transition-centre/sprints/sprint2/img/smart-goals/447782686.png"
+      alt="Screen" usemap="#screenmap1" width="1441px" v-show="showScreen1">
+
+    <map name="screenmap1">
+      <area shape="rect" coords="1165,115,1292,165" alt="Main" href="../../">
+      <router-link to="/">
+        <area shape="rect" coords="151,172,356,189" alt="MainPortal">
+      </router-link>
+      <router-link to="/plan">
+        <area shape="rect" coords="388,171,530,188" alt="ChecklistPlan">
+      </router-link>
+      <area shape="rect" coords="145,515,1136,2452" alt="FillGoals" @click="fillGoals">
+    </map>
+
+    <img id="screen2" src="https://code-for-canada.github.io/transition-centre/sprints/sprint2/img/smart-goals/447782687.png"
+      alt="Screen" usemap="#screenmap2" width="1441px" v-show="showScreen2">
+
+    <map name="screenmap2">
+      <area shape="rect" coords="1165,115,1292,165" alt="Main" href="../../">
+      <router-link to="/">
+        <area shape="rect" coords="151,172,356,189" alt="MainPortal">
+      </router-link>
+      <router-link to="/plan">
+        <area shape="rect" coords="388,171,530,188" alt="ChecklistPlan">
+      </router-link>
+      <router-link to="/plan2">
+        <area shape="rect" coords="147,2497,249,2541" alt="SaveGoals">
+      </router-link>
+    </map>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "members-portal",
+    data() {
+       return {
+          showScreen1: true,
+          showScreen2: false,
+        }
+    },
+    methods: {
+      fillGoals() {
+        this.showScreen1 = false;
+        this.showScreen2 = true;
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  body {
+    margin: 0;
+    text-align: center;
+  }
+
+  area {
+    cursor: pointer;
+  }
+</style>
