e4fead755e0d8b4003892999e11e3296e5d661e2
kakcura <korhan.akcura@codefor.ca>
2021-05-06 03:52:40 -0400

    Sprint 5 - Implement the wireframe for the transition plan 1

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from "axios"
 
 // Do not show production tip on startup.
 Vue.config.productionTip = false
+
+// Alias axios to Vue.prototype.axios for
+// calling it from any component.
+Vue.prototype.axios = axios;
 
 // Show accessibility issues at console in dev mode.
 if (process.env.NODE_ENV === 'development') {
